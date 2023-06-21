a7ca7d4c3dafa261b26ce2760933983c6dc0848a
S. Ulaşcan Kılıç <ulascank1@gmail.com>
2023-06-21 17:49:41 +0300

    initial commit

---
+++ jpa-hibernate/src/main/java/com/ulascan/jpahibernate/JpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.ulascan.jpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateApplication.class, args);
+	}
+
+}
---
+++ jpa-hibernate/src/test/java/com/ulascan/jpahibernate/JpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulascan.jpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
